0fb721d56d6f3bacd68c16bd8afb8bce13e34448
shaheermudassar <124002206+shaheermudassar@users.noreply.github.com>
2023-03-02 11:53:10 +0500

    Update DSA Array.cpp

--- DSA Array.cpp
+++ DSA Array.cpp
@@ -14,6 +14,28 @@ void addValue(int value) {
     arr[size++] = value;
     cout << "Value added successfully." << endl;
 }
+void addValueAtStart(int value) {
+    if (size == MAX_SIZE) {
+        cout << "Array is full." << endl;
+        return;
+    }
+    for (int i = size - 1; i >= 0; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[0] = value;
+    size++;
+    cout << "Value added at the start of the array." << endl;
+}
+void addValueAtEnd(int value) {
+    if (size == MAX_SIZE) {
+        cout << "Array is full." << endl;
+        return;
+    }
+    arr[size] = value;
+    size++;
+    cout << "Value added at the end of the array." << endl;
+}
+
 
 void deleteValue(int value) {
     int pos = -1;
@@ -32,6 +54,25 @@ void deleteValue(int value) {
     }
     size--;
     cout << "Value deleted successfully." << endl;
+}
+void deleteValueFromStart() {
+    if (size == 0) {
+        cout << "Array is empty." << endl;
+        return;
+    }
+    for (int i = 0; i < size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    size--;
+    cout << "Value deleted from the start of the array." << endl;
+}
+void deleteValueFromEnd() {
+    if (size == 0) {
+        cout << "Array is empty." << endl;
+        return;
+    }
+    size--;
+    cout << "Value deleted from the end of the array." << endl;
 }
 
 void insertValue(int value, int pos) {
@@ -60,9 +101,13 @@ int main() {
 
     while (true) {
         cout << "1. Add value to array" << endl;
-        cout << "2. Delete value from array" << endl;
-        cout << "3. Insert value in array" << endl;
-        cout << "4. Quit" << endl;
+        cout << "2. Add value at start of array" << endl;
+        cout << "3. Add value at end of array" << endl;
+        cout << "4. Delete value from array" << endl;
+        cout << "5. Delete value from start of array" << endl;
+        cout << "6. Delete value from end of array" << endl;
+        cout << "7. Insert value in array" << endl;
+        cout << "8. Quit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -75,11 +120,31 @@ int main() {
                 break;
             case 2:
             	system("cls");
+                cout << "Enter the value to be added at start: ";
+                cin >> value;
+                addValueAtStart(value);
+                break;   
+            case 3:
+            	system("cls");
+                cout << "Enter the value to be added at End: ";
+                cin >> value;
+                addValueAtEnd(value);
+                break;      
+            case 4:
+            	system("cls");
                 cout << "Enter the value to be deleted: ";
                 cin >> value;
                 deleteValue(value);
                 break;
-            case 3:
+            case 5:
+                deleteValueFromStart();
+                system("cls");
+                break;
+            case 6:
+                deleteValueFromEnd();
+                system("cls");
+                break;    
+            case 7:
             	system("cls");
                 cout << "Enter the value to be inserted: ";
                 cin >> value;
@@ -87,7 +152,7 @@ int main() {
                 cin >> pos;
                 insertValue(value, pos);
                 break;
-            case 4:
+            case 8:
                 return 0;
             default:
                 cout << "Invalid choice." << endl;
